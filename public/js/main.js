56b7295efb2676a7f310fdeed9d6b299973b135a
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-03 23:39:57 -0400

    setup socket.io

---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+//we have access to socket because of the script tag in the chat.html file
+const socket = io()
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+//we need to access this directly in order to use socket.io
+const http = require("http");
+const server = http.createServer(app);
+//set static folder
+const socketio = require("socket.io");
+const io = socketio(server);
 
+//this is how the backend can access the front end. Set static folder.
+app.use(express.static(path.join(__dirname, "public")));
+
+//run when a client connects
+io.on("connection", (socket) => {
+  console.log("new web socket connection...");
+});
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+ 
