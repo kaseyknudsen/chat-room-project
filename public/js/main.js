2a9c0211c86da9bd19c19f670e78a0821262fdc8
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-04 14:03:42 -0400

    added message to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,29 @@
 //we have access to socket because of the script tag in the chat.html file
 
-const chatForm = document.getElementById('chat-form')
+const chatForm = document.getElementById("chat-form");
 
 const socket = io();
+//message from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+});
 
-socket.on('message', message => {
-    console.log(message)
-} )
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //getting the msg text from the id in the input element
+  const msg = e.target.elements.msg.value;
+  console.log(msg);
+  //emit message to server
+  socket.emit("chatMessage", msg);
+});
 
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    //getting the msg text from the id in the input element
-    const msg = e.target.elements.msg.value;
-    console.log(msg)
-    //emit message to server
-    socket.emit('chatMessage', msg)
-})
+//output message to DOM
+const outputMessage = (message) => {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="text">
+   ${message}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+};
--- server.js
+++ server.js
@@ -28,13 +28,12 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     io.emit("message", "A user has left the chat");
   });
-//   io.emit();
+  //   io.emit();
 
-//listen for chat message
-socket.on('chatMessage', msg => {
-    console.log(msg)
-})
-
+  //listen for chat message
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 const PORT = 3000 || process.env.PORT;
 
