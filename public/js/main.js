ae28b3e0d1ea5532f174f675b3059452054d3902
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-06 18:56:07 -0400

    added focus and clear

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,27 @@
 //we have access to socket because of the script tag in the chat.html file
 
 const chatForm = document.getElementById("chat-form");
-
+const chatMessages = document.querySelector(".chat-messages");
 const socket = io();
 //message from server
 socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
+
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   //getting the msg text from the id in the input element
   const msg = e.target.elements.msg.value;
-  console.log(msg);
   //emit message to server
   socket.emit("chatMessage", msg);
+  //clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
-
 //output message to DOM
 const outputMessage = (message) => {
   const div = document.createElement("div");
